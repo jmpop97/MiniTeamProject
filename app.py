7fa67ff3deb3b60ea5a5c488ad06219508692c33
jmpop97 <pjm970128@gmail.com>
2023-03-10 11:54:54 +0900

    add fix

--- app.py
+++ app.py
@@ -98,10 +98,48 @@ def matjip_post():
 
     return jsonify({'msg':'저장완료!'})
 
-#@app.route('/write/<id>')
-#def write_fix(id):
-#    return render_template('write.html',Resid=id)
+@app.route('/fix/<id>')
+def write_fix(id):
+    return render_template('fix.html',Resid=id)
 
+@app.route("/fix", methods=["POST"])
+def matjip_fix():
+    id_receive = request.form['id_give']
+    url_receive = request.form['url_give']
+    #가게이름
+    stname_receive = request.form['stname_give']
+    #가게소개
+    stintro_receive = request.form['stintro_give']
+    #주소 추후 주소API, 카카오맵API
+    stadd_receive = request.form['stadd_give']
+
+    ststar_receive = request.form['ststar_give']
+    comment_receive = request.form['comment_give']
+    #평균 가격이 아니라 가격
+    price_receive = request.form['price_give']
+
+    # headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    # data = requests.get(url_receive,headers=headers)
+
+    # soup = BeautifulSoup(data.text, 'html.parser')
+
+    # 여기에 코딩을 해서 meta tag를 먼저 가져와보겠습니다.
+    # ogimage = soup.select_one('meta[property="og:image"]')['content']
+    
+    doc = {
+        'url': url_receive,
+        'stname': stname_receive,
+        'stintro': stintro_receive,
+        'stadd': stadd_receive,
+        'ststar': ststar_receive,
+        'comment': comment_receive,
+        'price': price_receive        
+    }
+
+    # 저장
+    db.matjip.update_one({'_id':id_receive},doc)
+
+    return jsonify({'msg':'저장완료!'})
 
 @app.route('/matgo_list_detail/<id>')
 def matgo_list_detail(id):
